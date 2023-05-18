24380252cc180c5453772c72e366ef980b4de4a3
Olzhas1997 <86761912+Olzhas1997@users.noreply.github.com>
2023-05-18 21:31:59 +0500

    Update main.py
    Проверка облачного streamlit

--- main.py
+++ main.py
@@ -9,3 +9,14 @@ df = pd.read_csv('titanic.csv')
 st.title("Лабораторная работа 7\nКоманда: Дударев А., Петров Д., Ережепов О.")
 
 choice: int = int(st.selectbox("Выберите номер задания: ", [1, 2, 3]))
+ 
+if choice != None:
+    match choice:
+        case 1:
+            first_func.avg_price_output(df)
+        case 2:
+            second_func.avg_age_output(df)
+        case 3:
+            third_func.gender_count_output(df)
+else:
+    pass
