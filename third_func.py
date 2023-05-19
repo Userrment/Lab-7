dd1d8fc8656361f02b12feba7144eb92d177f64f
Olzhas1997 <86761912+Olzhas1997@users.noreply.github.com>
2023-05-19 09:34:40 +0500

    Update third_func.py
    Проверка фикса

--- third_func.py
+++ third_func.py
@@ -10,28 +10,25 @@ def gender_count_output(df: pd.DataFrame) -> None:
 
     text: str = "Количество пассажиров по классам — "
     choice: int
+    
+    if pass_class == "Класс 1":
+        text += f"\"{choice_list[0]}\""
+        choice = 1
+    elif pass_class == "Класс 2":
+        text += f"\"{choice_list[1]}\""
+        choice = 2
+    elif pass_class == "Класс 3":
+        text += f"\"{choice_list[2]}\""
+        choice = 3
 
-    match pass_class:
-        case "Класс 1":
-            text += f"\"{choice_list[0]}\""
-            choice = 1
-        case "Класс 2":
-            text += f"\"{choice_list[1]}\""
-            choice = 2
-        case "Класс 3":
-            text += f"\"{choice_list[2]}\""
-            choice = 3
     df_slice = gender_count(choice, df)
 
     st.write(f"{text}:")
 
     for col_name, item in df_slice.items():
         gender: str
-
-        match col_name: 
-            case "female":
-                gender = "Женщины"
-            case "male":
-                gender = "Мужчины"
+        
+        if col_name == "female": gender = "Женщины"
+        elif col_name == "male": gender = "Мужчины"
 
         st.write(f"{gender} — {item}")
